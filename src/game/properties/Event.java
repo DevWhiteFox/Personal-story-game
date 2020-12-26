f5203a4a63b6f88f6fe4016f60c9039604a30893
DevWhiteFox <DevFoxy@protonmail.com>
2020-12-26 13:45:05 +0100

    Package manage: moved classes to simplify

---
+++ src/game/properties/Event.java
@@ -0,0 +1,46 @@
+package game.properties;
+
+import game.event.Option;
+
+import java.util.ArrayList;
+
+import java.util.Map;
+
+public class Event {
+    private String nameEvent;
+    private String nameImage;
+    private ArrayList<Option> options;
+    private Map<Character, Integer> consequences;
+
+    public String getNameEvent() {
+        return nameEvent;
+    }
+
+    public void setNameEvent(String nameEvent) {
+        this.nameEvent = nameEvent;
+    }
+
+    public String getNameImage() {
+        return nameImage;
+    }
+
+    public void setNameImage(String nameImage) {
+        this.nameImage = nameImage;
+    }
+
+    public ArrayList<Option> getOptions() {
+        return options;
+    }
+
+    public void setOptions(ArrayList<Option> options) {
+        this.options = options;
+    }
+
+    public Map<Character, Integer> getConsequences() {
+        return consequences;
+    }
+
+    public void setConsequences(Map<Character, Integer> consequences) {
+        this.consequences = consequences;
+    }
+}
