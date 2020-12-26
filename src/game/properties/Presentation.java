b22223d222b9bd0e2cb5595cbb0dce0fdb446c7f
DevWhiteFox <DevFoxy@protonmail.com>
2020-12-26 17:33:17 +0100

    Class&Package manage: renamed package, imported ImageManage, added image field in Presentation

--- src/game/properties/Presentation.java
+++ src/game/properties/Presentation.java
@@ -1,18 +1,29 @@
 package game.properties;
 
+import javax.swing.*;
+
 public class Presentation {
     private String name;
     private String description;
+    private ImageIcon image;
 
     Presentation(Presentation presentation){
         name = presentation.name;
         description = presentation.description;
+        image = presentation.image;
     }
 
     Presentation(String name, String description){
+        this(name,description, null);
+    }
+
+    Presentation(String name, String description, ImageIcon image){
         this.name = name;
         this.description = description;
+        this.image = image;
     }
+
+
 
     public String getName() {
         return name;
@@ -20,5 +31,9 @@ public class Presentation {
 
     public String getDescription() {
         return description;
+    }
+
+    public ImageIcon getImage() {
+        return image;
     }
 }
---
+++ src/game/story/Story.java
@@ -0,0 +1,10 @@
+package game.story;
+
+import game.properties.Scenario;
+
+import java.util.ArrayList;
+
+public class Story {
+    private ArrayList<Scenario> scenarios = new ArrayList<>();
+
+}
