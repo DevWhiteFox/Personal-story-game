344a4ddb823dd0fcc991065ec66040783315e33d
DevFoxy <DevFoxy@protonmail.com>
2020-12-26 12:55:19 +0100

    Class manage: Add consequences Map, We need to understand how to use them as commands, Still no tests

--- src/game/event/EventProperties.java
+++ src/game/event/EventProperties.java
@@ -1,17 +1,25 @@
 package game.event;
 
 import java.util.List;
+import java.util.Map;
 
 class EventProperties {
-    private String nameEvent = null;
-    private String nameImage = null;
-    private List<Option> options = null;
+    private String nameEvent;
+    private String nameImage;
+    private List<Option> options;
+    private Map<Character, Integer> consequences;
 
-    EventProperties(String nameEvent, String nameImage, List<Option> options) {
+    EventProperties(String nameEvent, String nameImage, List<Option> options, Map<Character, Integer> consequences) {
         this.nameEvent = nameEvent;
         this.nameImage = nameImage;
         this.options = options;
+        this.consequences = consequences;
     }
+
+    EventProperties(String nameEvent){
+        this.nameEvent = nameEvent;
+    }
+
 
     //temp constructor
     EventProperties(){}
@@ -26,5 +34,9 @@ class EventProperties {
 
     public List<Option> getOptions() {
         return options;
+    }
+
+    public Map<Character, Integer> getConsequences() {
+        return consequences;
     }
 }
